a2d8335e66a30ef24df27c993f4c7cbc62dba27b
valed-dm <dmvaled@gmail.com>
2021-09-10 20:39:28 +0300

    db connection established

--- api.js
+++ api.js
@@ -1,1 +1,22 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+
+// Connecting to database
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  },
+  function(error) {
+    if (error) {
+      console.log("Error!" + error);
+    }
+  }
+);
+
+module.exports = router;
 
