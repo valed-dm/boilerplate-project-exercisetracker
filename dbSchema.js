1d0c5b75731cf1d8a35235de6ae0199946fa464d
valed-dm <dmvaled@gmail.com>
2021-09-10 20:41:39 +0300

    mongoose model created

--- dbSchema.js
+++ dbSchema.js
@@ -1,1 +1,14 @@
+var mongoose = require("mongoose");
 
+var userSchema = new mongoose.Schema({
+  username: String,
+  created: Date,
+  count: Number,
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+}]
+});
+
+module.exports = mongoose.model("userLIST", userSchema, "users");
